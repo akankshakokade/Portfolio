50d01ebe1de1ecf1f938111f7fc83a48c2a45a8e
Akanksha Kokade <akankshakokade2001@gmail.com>
2024-01-12 11:08:59 +0530

    done

---
+++ Portfolio/src/data/constants.js
@@ -0,0 +1,209 @@
+export const Bio = {
+  name: "Akanksha Kokade",
+  roles: [
+    "Web Developer",
+    "Full Stack Developer",
+    "Programmer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/Akankshakokade",
+  resume:
+    "https://drive.google.com/file/d/16oEEmI-G0reOfwWpvk-6R96mJmHXEo9C/view?usp=drive_link",
+  linkedin: "https://www.linkedin.com/in/akanksha-kokade-b511081a6",
+  twitter: "https://twitter.com/Akankshakokade",
+  insta: "https://www.instagram.com/akku_9795",
+  facebook: "https://www.facebook.com/profile.php?id=61555208024182",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Java",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+      },
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "PHP",
+        image:
+          "https://www.php.net//images/logos/new-php-logo.svg",
+      },
+            {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+      // {
+      //   name: "Firebase",
+      //   image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+      // },
+    ],
+  },
+  {
+    title: "Android",
+    skills: [
+      {
+        name: "Java",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+      },
+      {
+        name: "XML",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBMw6_RdwKQ9bDFfnKDX1iwMl4bVJEvd9PP53XuIw&s",
+      },
+      {
+        name: "Android Studio",
+        image:
+          "https://developer.android.com/static/studio/images/new-studio-logo-1_1920.png",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://drive.google.com/file/d/1jl-v0oSks78QbX4jk_0GDjbj6wV9Wj8X/view?usp=drive_link",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+    ],
+  },
+];
+
+
+export const education = [
+  {
+    id: 0,
+    img: "https://images.collegedunia.com/public/college_data/images/logos/1477030899SIMCAlogo.jpg?h=80&w=80&mode=stretch",
+    school: "Asma Institute of Management and Computer Application,Shivane",
+    date: "  Nov 2023 - May 2025",
+    grade:"Not updated",
+    desc: "I am currently pursuing a Master's degree in Computer application at Asma Institute of Management and Computer Application, Shivane. I have completed 1 semester . In the academic curriculum i have learn various subjects such as Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others. where I am learning, improving skills and working on exciting projects.",
+    degree: "Master of Computer Application",
+  },
+  {
+    id: 1,
+    img: "https://www.ctboracollege.edu.in/images/College-logo1.jpg",
+    school: "C. T. Bora Arts, Science & Commerce college, Shirur, Pune",
+    date: "Apr 2019 - Apr 2022",
+    grade: "8.80 CGPA",
+    desc: "I completed my Bachelor's degree in Computer Science at C. T. Bora Arts, Science & Commerce college, Shirur. Where i learn programming languages and computer fundamental concepts and also various skills such as C, java, php, frontend technologies and so on",
+    degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
+  },
+  {
+    id: 2,
+    img: "https://www.ctboracollege.edu.in/images/College-logo1.jpg",
+    school: "Shri Mallikarjun Jr. college, Nhavare, Shirur",
+    date: "Apr 2017 - Apr 2019",
+    grade: "62.62%",
+    desc: "I completed my class 12 high school education at Shri Mallikarjun Jr. college, Nhavare. where I studied Science with PCMB Group.",
+    degree: "HSC(XII), Science with PCMB Group",
+  },
+  {
+    id: 3,
+    img: "https://www.ctboracollege.edu.in/images/College-logo1.jpg",
+    school: "Shri Mallikarjun Vidyalaya Nhavare , Shirur, Pune.",
+    date: "Apr 2016 - Apr 2017",
+    grade: "87.80%",
+    desc: "I completed my class 10 education at Shri Mallikarjun Vidyalaya Nhavare. where I completed my secondary education",
+    degree: "SSC(X), Science",
+  },
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "Online Feedback System",
+    date: "Nov 2022 - Mar 2023",
+    description:
+      " The Online Feedback system is Java Project. The Feedback system is developed to manage Feedback of students for teacher in IT Institutes. These systems will ultimately allow you to better management of institute",
+    image:
+      "https://drive.google.com/file/d/1j1nMb4B3RZZx39tM6Wj0CoT2IafXX6L0/view?usp=drive_link",
+    tags: ["HTML5 & CSS3",  "Bootstrap","Javascript", "Java"],
+    category: "web app",
+    github: "https://github.com/Akankshakokade/OnlineFeedbackSystem",
+  },
+  {
+    id: 1,
+    title: "Expense Tracker",
+    date: "May 2023 - Jul 2023",
+    description:
+      "The Expense Tracker has been developed to manage the expenses in daily life. This system helps to manage the expenses. This system also helps to modify content as per the date,Expense Name",
+    image:
+      "https://github.com/HusenMujawar/Agri-Boost/blob/main/Screenshot%202024-01-08%20141022.png?raw=true",
+    tags: ["Html", "CSS", "Javascript", "Java", "MYSQL"],
+    category: "web app",
+    github: "https://github.com/HusenMujawar/Agri-Boost.git",
+  },
+  {
+    id: 2,
+    title: "Portfolio website",
+    date: "Jan 2024",
+    description:
+      "A portfolio react based responsive website. This project showcases my skills, projects, and experiences as a web developer.",
+    image:
+      "https://github.com/HusenMujawar/Portfolio/blob/main/src/images/portfolioimage.png?raw=true",
+    tags: ["HTML5", "CSS", "Javascript", "ReactJS"],
+    category: "web app",
+    github: "https://github.com/HusenMujawar/Portfolio.git",
+  },
+ 
+];
+
